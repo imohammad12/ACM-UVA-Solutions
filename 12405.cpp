89403132b8d4a9c42d277f522a0553ca5fe7fb5a
imohammad12 <mohamad.dehghan12@yahoo.com>
2017-07-02 04:17:31 +0430

    Create 12405.cpp

---
+++ 12405.cpp
@@ -0,0 +1,67 @@
+//                                                  In The Name Of God
+//                                              programmer:Mohammad Dehghan
+#include <iostream>
+using namespace std;
+
+#include <vector>
+#include <set>
+#include <string>
+#include <string.h>
+#include <math.h>
+#include <map>
+#include <iomanip>
+#include <queue>
+#include <algorithm>
+#include <sstream>
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<string> vs;
+typedef pair<int, int> ii;
+typedef pair<int, ii> iii;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<double> vd;
+typedef vector<vd> vvd;
+typedef vector<vvi> vvvi;
+typedef vector<vvvi> vvvvi;
+typedef vector<ii> vii;
+typedef vector<iii> viii;
+typedef vector<vii> vvii;
+typedef vector<vvii> vvvii;
+typedef vector<vector<viii> > vvviii;
+typedef vector<vector<iii> > vviii;
+typedef set<int> si;
+typedef vector<si> vsi;
+typedef pair<double, double> dd;
+typedef vector<dd> vdd;
+
+#define inf 1000000000
+#define eps 1e-9
+#define pi acos(-1.0) // alternative #define pi (2.0 * acos(0.0))
+#define F first
+#define S second
+#define pb push_back
+
+
+
+int main () {
+    int tc;
+    cin >> tc;
+     for(int kk = 1 ; kk<= tc ; kk++){
+        int num;
+        cin >> num;
+        string S;
+        cin >> S;
+        int answer =0;
+        for (int i = 0; i < S.size(); ++i) {
+            while (S[i] == '#')
+                i++;
+            if(S[i] == '.')
+                answer++;
+            i += 2;
+        }
+        cout<<"Case "<< kk << ": "<< answer<<endl;
+    }
+}
+
