0037b88c95e6a18ba437c076cfcd07a35029eb15
imohammad12 <mohamad.dehghan12@yahoo.com>
2017-07-23 14:32:09 +0430

    Create 10656.cpp

---
+++ 10656.cpp
@@ -0,0 +1,71 @@
+//                                                  In The Name Of God
+//                                              programmer:Mohammad Dehghan
+#include <iostream>
+using namespace std;
+
+#include <vector>
+#include <set>
+#include <string>
+#include <string.h>
+#include <math.h>
+#include <map>
+#include <iomanip>
+#include <queue>
+#include <algorithm>
+#include <sstream>
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<string> vs;
+typedef pair<int, int> ii;
+typedef pair<int, ii> iii;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<double> vd;
+typedef vector<vd> vvd;
+typedef vector<vvi> vvvi;
+typedef vector<vvvi> vvvvi;
+typedef vector<ii> vii;
+typedef vector<iii> viii;
+typedef vector<vii> vvii;
+typedef vector<vvii> vvvii;
+typedef vector<vector<viii> > vvviii;
+typedef vector<vector<iii> > vviii;
+typedef set<int> si;
+typedef vector<si> vsi;
+typedef pair<double, double> dd;
+typedef vector<dd> vdd;
+
+#define inf 1000000000
+#define eps 1e-9
+#define pi acos(-1.0) // alternative #define pi (2.0 * acos(0.0))
+#define F first
+#define S second
+#define pb push_back
+
+
+
+int main () {
+   int num;
+    while (cin >> num && num){
+        bool first = true , check2 = true;
+        for(int i = 0 ; i< num ; i++){
+            int temp;
+            cin >> temp;
+            if(first && temp){
+                cout << temp;
+                first = false;
+                check2 = false;
+                continue;
+            }
+            if(temp) {
+                cout<<" " <<temp;
+                check2 = false;
+            }
+        }
+        if(check2){
+            cout << 0;
+        }
+        cout << endl;
+    }
+}
